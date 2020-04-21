a3f8fb18b1c3287e0e7684b4f6cacb4d6c8c2b63
Ramakrishna <Ramakrishna@LAPTOP-VT80RB39>
2020-04-21 10:17:22 +0530

    google search maven project told by mahemad yaseen sir

---
+++ google-search/src/test/java/com/google/LandingPageTest.java
@@ -0,0 +1,18 @@
+package com.google;
+
+import org.testng.annotations.Test;
+
+public class LandingPageTest 
+{
+	 @Test
+	    public void Landing()
+	    {
+	    	System.out.println("landind is done");
+	    }
+	 @Test
+	    public void Landing1()
+	    {
+	    	System.out.println("landind1 is done");
+	    }
+	    
+}
---
+++ google-search/src/test/java/com/google/SearchResultTest.java
@@ -0,0 +1,19 @@
+package com.google;
+
+import org.testng.annotations.Test;
+
+public class SearchResultTest 
+{
+    @Test
+    public void Search()
+    {
+    	System.out.println("search is done");
+    }
+    
+    @Test
+    public void Search2()
+    {
+    	System.out.println("search2 is done");
+    }
+    
+}
